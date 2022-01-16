7e8b4e8ce47c5afc6f0b5b967d3a264d7a11d1d3
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-01-16 12:55:55 +0330

    Solve some problems

--- src/Projects.js
+++ src/Projects.js
@@ -2,6 +2,15 @@ import React from "react";
 
 import "./styles/Projects.css";
 
+const images = [
+  { id: "1", imageName: "Showcase-06-705x705.png" },
+  { id: "2", imageName: "Showcase-05-705x705.png" },
+  { id: "3", imageName: "Showcase-04-705x705.png" },
+  { id: "4", imageName: "Showcase-03-705x705.png" },
+  { id: "5", imageName: "Showcase-02-705x705.png" },
+  { id: "6", imageName: "Showcase-01-705x705.png" },
+];
+
 const Projects = () => {
   return (
     <div className="Projects">
@@ -11,42 +20,14 @@ const Projects = () => {
       </h2>
 
       <div className="Projects__items">
-        <div>
-          <img
-            src={require("./img/Showcase-06-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
-        <div>
-          <img
-            src={require("./img/Showcase-05-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
-        <div>
-          <img
-            src={require("./img/Showcase-04-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
-        <div>
-          <img
-            src={require("./img/Showcase-03-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
-        <div>
-          <img
-            src={require("./img/Showcase-02-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
-        <div>
-          <img
-            src={require("./img/Showcase-01-705x705.png")}
-            alt="Projects item"
-          />
-        </div>
+        {images.map((image) => (
+          <div key={image.id}>
+            <img
+              src={require(`./img/${image.imageName}`)}
+              alt="Projects item"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
